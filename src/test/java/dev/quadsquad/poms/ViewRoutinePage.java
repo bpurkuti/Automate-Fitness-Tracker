a2e40f168fc39f47465cff1f53719fb781cd914a
wolfryan97 <wolf.ryan97@gmail.com>
2021-06-25 11:31:21 -0400

    Added more POMs
    added poms and variables for all planned pom elements

---
+++ src/test/java/dev/quadsquad/poms/ViewRoutinePage.java
@@ -0,0 +1,47 @@
+package dev.quadsquad.poms;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class ViewRoutinePage {
+
+    private WebDriver driver;
+
+    public ViewRoutinePage(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    //temp ids --- NOT YET IMPL ---
+    @FindBy(id = "logout")
+    public WebElement logout;
+
+    @FindBy(id = "start")
+    public WebElement start;
+
+    @FindBy(id = "exercise")
+    public WebElement exercise;
+
+    @FindBy(id = "description")
+    public WebElement description;
+
+    @FindBy(id = "video")
+    public WebElement video;
+
+    @FindBy(id = "duration")
+    public WebElement duration;
+
+    @FindBy(id = "weight")
+    public WebElement weight;
+
+    @FindBy(id = "reps")
+    public WebElement reps;
+
+    @FindBy(id = "next-exercise")
+    public WebElement nextExercise;
+
+    @FindBy(id = "complete")
+    public WebElement complete;
+}
